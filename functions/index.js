e5a2de41b8a53a3f7e68f3860c8c520d25fc040e
Fernando Alvarez <fernando_0694@hotmail.com>
2025-01-08 10:56:49 -0400

    setup firebase ready

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,20 @@
-var admin = require("firebase-admin");
+const functions = require("firebase-functions");
+const bodyParser = require("body-parser");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
 
-var serviceAccount = require("./serviceAccount.json");
+// Configuración de serviceAccount
+const serviceAccount = require("./serviceAccount.json");
 
+// Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
+// CRUD users
+const users = express();
+users.use(cors({ origin: true }));
+users.use(require("./src/routes/users/users.routes"));
+
+exports.users = functions.https.onRequest(users);
