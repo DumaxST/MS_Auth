8560ea0062d25d8dc91dd38bfca6182ae7844bde
ZairDinahet <dinahetzair@gmail.com>
2025-02-18 15:40:15 -0300

    feat(utils): add utility functions for error handling and response formatting

--- functions/src/utils/index.js
+++ functions/src/utils/index.js
@@ -1,6 +1,4 @@
 module.exports = {
   response: require('./response'),
   cachedAsync: require('./catchedAsync'),
-  errors: require('./errors'),
-  languageTranslation: require('./languageTranslation')
 }
